feab3cee16cac7ea684202fac18185bcbcd513d7
orjanen <180337@stud.hvl.no>
2021-04-07 10:06:00 +0200

    simple setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from "react";
+import {Container, Header, Input, Button} from "semantic-ui-react";
 
 function App() {
+
+  const [data, setData] = useState("")
+    const [input, setInput] = useState("")
+
+    const onChangeInput = (event) =>{
+      setInput(event.target.value);
+    }
+
+    const onClickGetSentiment = async () =>{
+      if(input === "") return
+      try{
+          await axios.post('https://dat255sentiment-backend.herokuapp.com/sentiment', {"sentence": input})
+              .then(response => setData(response.data.sentiment))
+      }catch(e){
+          throw e
+      }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container textAlign='center'>
+        <Header as='h1' style={{marginTop:'200px'}}>
+          Sentiment application
+            <Header.Subheader>
+                Enter a Twitter username
+            </Header.Subheader>
+        </Header>
+          <Input
+              placeholder='Username'
+              onChange={(e) => {onChangeInput(e)}}
+          ></Input>
+          <Button
+              primary
+              onClick={() => onClickGetSentiment()}
+          >Get sentiment</Button>
+
+          <Header>{data}</Header>
+        </Container>
   );
 }
 
