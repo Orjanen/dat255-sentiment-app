2c5594bacba601b09e0c296d39536399370702d7
orjanen <180337@stud.hvl.no>
2021-04-07 16:47:58 +0200

    added cards

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
 import './App.css';
 import axios from 'axios'
-import {useEffect, useState} from "react";
-import {Container, Header, Input, Button} from "semantic-ui-react";
+import {useState} from "react";
+import {Button, Container, Grid, Header, Input} from "semantic-ui-react";
+import TweetCard from "./components/TweetCard";
 
 function App() {
 
-  const [data, setData] = useState("")
+
+    const [data, setData] = useState("")
     const [input, setInput] = useState("")
+    const [tweets, setTweets] = useState([])
 
-    const onChangeInput = (event) =>{
-      setInput(event.target.value);
+    const onClickGetSentiment = async () => {
+        const data = []
+        try {
+            await axios.post('https://dat255sentiment-backend.herokuapp.com/gettweets', {"name": input})
+                .then(response => {
+                    for(let i in response.data) {
+                        data.push(response.data[i])
+                    }
+                })
+            setTweets(data)
+        } catch (e) {
+            throw e
+        }
     }
 
-    const onClickGetSentiment = async () =>{
-      if(input === "") return
-      try{
-          await axios.post('https://dat255sentiment-backend.herokuapp.com/sentiment', {"sentence": input})
-              .then(response => setData(response.data.sentiment))
-      }catch(e){
-          throw e
-      }
+    const onChangeInput = (event) => {
+        setInput(event.target.value);
     }
 
-  return (
-      <Container textAlign='center'>
-        <Header as='h1' style={{marginTop:'200px'}}>
-          Sentiment application
-            <Header.Subheader>
-                Enter a Twitter username
-            </Header.Subheader>
-        </Header>
-          <Input
-              placeholder='Username'
-              onChange={(e) => {onChangeInput(e)}}
-          ></Input>
-          <Button
-              primary
-              onClick={() => onClickGetSentiment()}
-          >Get sentiment</Button>
+    return (
+        <Container textAlign='center'>
+            <Header as='h1' style={{marginTop: '200px'}}>
+                Sentiment application
+                <Header.Subheader>
+                    Enter a Twitter username
+                </Header.Subheader>
+            </Header>
+            <Input
+                placeholder='Username'
+                onChange={(e) => {
+                    onChangeInput(e)
+                }}
+            ></Input>
+            <Button
+                primary
+                onClick={() => onClickGetSentiment()}
+            >Get sentiment</Button>
 
-          <Header>{data}</Header>
+            <Header>{data}</Header>
+            <Grid columns='equal'>
+                <Grid.Row columns={4}>
+                    {tweets.map(item => {
+                        return (
+                            <Grid.Column key={item.id}>
+                                <TweetCard
+                                    name={input}
+                                    tweets={item.tweets}
+                                    likes={item.likes}
+                                    retweet={item.retweet}
+                                    sentiment={item.sentiment}
+                                />
+
+                            </Grid.Column>
+                        )
+                    })}
+                </Grid.Row>
+            </Grid>
+
         </Container>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/TweetCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Card, Icon, Button, Header} from 'semantic-ui-react'
+
+const TweetCard = (props) => {
+
+    const name = props.name
+    return (
+        <Card style={{marginTop:"20px"}}>
+            <Card.Content>
+                <Card.Header>{name}</Card.Header>
+                <Card.Meta>{props.date}</Card.Meta>
+                <Card.Description>
+                    {props.tweets}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <a style={{marginRight:"10px"}}>
+                    <Icon name='heart' />
+                    {`${props.likes} Likes`}
+                </a>
+                <a>
+                    <Icon name='redo' />
+                    {`${props.likes} Retweets`}
+                </a>
+            </Card.Content>
+            <Card.Content extra>
+                <Header as='h5'>{`We belive the sentiment is ${props.sentiment}`}</Header>
+                <Button.Group>
+                    <Button   color='green'> Agree</Button>
+                    <Button basic negative> It's Wrong</Button>
+                </Button.Group>
+            </Card.Content>
+
+        </Card>
+    );
+}
+
+export default TweetCard;
